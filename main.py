2ac8592f62371e2bcc209f35b88ef9cd0c3e77b5
Patryk Gawronski <pzgawronski@gmail.com>
2022-02-12 10:18:01 +0100

    added QuizBrain class and its next_question method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,3 +9,7 @@ for datum in question_data:
     input_answer = datum["answer"]
     output_question = Question(input_text, input_answer)
     question_bank.append(output_question)
+
+quiz_brain = QuizBrain(question_bank)
+
+quiz_brain.next_question()
